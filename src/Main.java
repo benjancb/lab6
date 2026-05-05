57131f05d57a7caf374a07e307b51f95d07cce48
benjancb <b.carrasco.be@gmail.com>
2026-05-05 17:45:35 -0400

    termine el Lab6

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+        RegistroDisquera departamentoMarketing = RegistroDisquera.getInstance();
+        RegistroDisquera departamentoProduccion = RegistroDisquera.getInstance();
+
+        departamentoMarketing.registrarArtista("Dua Lipa");
+        departamentoMarketing.registrarArtista("The Weeknd");
+
+        departamentoProduccion.registrarArtista("Rosalía");
+
+        departamentoProduccion.mostrarArtistas();
+
+        System.out.println(departamentoMarketing == departamentoProduccion);
+    }
+}
